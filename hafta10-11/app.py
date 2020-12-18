d83e76a63007f247e4b20562974e5c8ea22d3fde
Caner BALIM <cnrblm@gmail.com>
2020-12-18 12:10:44 +0300

    hafta 11

--- hafta10-11/app.py
+++ hafta10-11/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask.globals import request
 from flask.templating import render_template
 from werkzeug.utils import redirect
+import uuid
+from yazi import Yazi
 
 app=Flask(__name__)
 
-def dosyayaKaydet(baslik,icerik,etiket,saat):
+def dosyayaKaydet(id,baslik,icerik,etiket,saat):
     with open("veriler.txt","a") as f:
-        f.write(baslik+"|"+icerik+"|"+etiket+"|"+saat+"\n")
+        f.write(str(id)+"|"+baslik+"|"+icerik+"|"+etiket+"|"+saat+"\n")
         f.close()
 
 def dosyadanOku():
@@ -21,10 +23,13 @@ def dosyadanOku():
     for satir in yaziListe:
         satir=satir.replace("\n","")
         yazi = satir.split("|")
-        sayfaGosterilecekList.append(yazi[0])
+        sayfaGosterilecekList.append(dosyayiSinifaAktar(yazi))
 
     return sayfaGosterilecekList
-    
+
+def dosyayiSinifaAktar(liste):
+    return Yazi(liste[0],liste[1],liste[2],liste[3],yayintarihi=liste[4])
+
 @app.route("/")
 def anasayfa():
     liste=dosyadanOku()
@@ -33,14 +38,25 @@ def anasayfa():
 @app.route("/yaziekle",methods=["POST","GET"])
 def yaziEkle():
     if request.method=="POST":
+        id=uuid.uuid1()
         baslik=request.form.get("inp_baslik")
         icerik=request.form.get("text_icerik")
         etiket=request.form.get("inp_etiket")
         saat=request.form.get("inp_tarih")
-        dosyayaKaydet(baslik,icerik,etiket,saat)
+        dosyayaKaydet(id,baslik,icerik,etiket,saat)
         return redirect("/")
     else:
         return render_template("yaziekle.html")
+
+@app.route("/detay/<id>")
+def detay(id):
+    gosterilecekYazi=None
+    yaziListe=dosyadanOku()
+    for yazi in yaziListe:
+        if yazi.id == id:
+            gosterilecekYazi=yazi
+            break
+    return render_template("detay.html",yaziDetay=gosterilecekYazi)
 
 
 if __name__ == "__main__":
---
+++ hafta10-11/yazi.py
@@ -0,0 +1,9 @@
+class Yazi:
+    def __init__(self,id,yazibaslik,yaziicerik,yazietiket,yayintarihi):
+        self.id=id
+        self.yazibaslik=yazibaslik
+        self.yaziicerik=yaziicerik
+        self.yazietiket=yazietiket
+        self.yayintarihi=yayintarihi
+    
+    
